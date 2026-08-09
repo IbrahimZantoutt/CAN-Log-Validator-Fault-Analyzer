a80d965e14b714fb65272cf11a8c1ad5797de754
Ibrahim Zantout <kingofdeath3636@gmail.com>
2026-08-09 17:42:35 +0300

    finish test_loader.py (end of feature/loader branch)

--- src/can_validator/loader.py
+++ src/can_validator/loader.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def load_log(file_path: str)-> list[dict]:
+def load_log(file_path: str, continueMode=False)-> list[dict]:
     '''
     load_log function: opens a csv file and gets the data as dicts and changes some datatypes and then returns a list with the data
 
@@ -23,14 +23,21 @@ def load_log(file_path: str)-> list[dict]:
                     # print(row)
                     listAll.append(row)
                 except ValueError:
-                    logger.warning(f"invalid hex characters skipping row: {row}")
+                    if(continueMode):
+                        logger.warning(f"invalid hex characters skipping row: {row}")
+                        continue
+                    else:
+                        raise ValueError(f"invalid hex data in row: {row}")
+                        
                     
     except FileNotFoundError:
         raise FileNotFoundError(f"file was not found at {file_path}")
     except PermissionError:
         raise PermissionError(f"permission error in file at {file_path}")
     except UnicodeDecodeError:
-        raise UnicodeDecodeError(f"decoder error for file at {file_path}")
+        raise ValueError(f"decoder error for file at {file_path}")
+    print(listAll)
     return listAll
 
 load_log("data/sample_log.csv")
+load_log("data/sample_log_invHex.csv",True)
---
+++ tests/test_loader.py
@@ -0,0 +1,25 @@
+import pytest
+
+import sys
+sys.path.append("src")
+from can_validator.loader import load_log
+
+
+@pytest.mark.parametrize("path",[
+    ("data/logs.csv"),
+    ("sample_log.csv"),
+    ("info/sample_log.csv"),
+])
+def test_file_not_found(path):
+    with pytest.raises(FileNotFoundError):
+        load_log(path)
+
+def test_invlid_hex():
+    with pytest.raises(ValueError):
+        load_log("data/sample_log_invHex.csv")
+
+def test_expected_dict():
+    assert load_log("data/sample_log.csv") == [{'timestamp': 0.0, 'message_id': 213, 'data': b'2\x00\xe6\x00'}, {'timestamp': 0.02, 'message_id': 213, 'data': b'3\x00\xe6\x00'}, {'timestamp': 0.04, 'message_id': 213, 'data': b'4\x00\xe6\x00'}, {'timestamp': 0.1, 'message_id': 126, 'data': b'\x05\x01\x00\x00\x00'}, {'timestamp':0.15, 'message_id': 78, 'data': b'2\xff\x04\x00'}, {'timestamp': 0.17, 'message_id': 78, 'data': b'2\xff\x04\x00'}, {'timestamp': 0.19, 'message_id': 78, 'data': b'2\xff\x04\x00'}]
+
+def test_return_good_data():
+    assert load_log("data/sample_log_invHex.csv",True) == [{'timestamp': 0.0, 'message_id': 213, 'data': b'2\x00\xe6\x00'}, {'timestamp': 0.02, 'message_id': 213, 'data': b'3\x00\xe6\x00'}, {'timestamp': 0.04, 'message_id': 213, 'data': b'4\x00\xe6\x00'}, {'timestamp': 0.1, 'message_id': 126, 'data': b'\x05\x01\x00\x00\x00'}, {'timestamp':0.15, 'message_id': 78, 'data': b'2\xff\x04\x00'}, {'timestamp': 0.17, 'message_id': 78, 'data': b'2\xff\x04\x00'}, {'timestamp': 0.19, 'message_id': 78, 'data': b'2\xff\x04\x00'}]
